b22bfb45c239e5437f40086634881edc39abee46
Ann <93719602+wangui-ann@users.noreply.github.com>
2026-02-13 10:56:47 +0300

    Change greeting message format in hello route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     secret = os.environ.get("SECRET_KEY", "NoSecret")
-    return f"Hello World! Secret: {secret}"
+    return f"Hello World... Secret: {secret}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
